a2a85b094aae63c6fd410e0aff6e72a6e6af917b
CindyChang0217 <102627093+CindyChang0217@users.noreply.github.com>
2022-10-14 12:28:21 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,12 +1,16 @@
 import java.net.URI;
 import java.util.ArrayList;
-class SearchEngine implements URLHandler{
+import java.io.IOException;
+class Handler implements URLHandler{
     ArrayList<String> str = new ArrayList<String>();
     public String handleRequest(URI url){
+        if (url.getPath().equals("/")) {
+            return "";}
         if (url.getPath().contains("/add")){
             String[] parameter = url.getQuery().split("=");
             if (parameter[0].equals("s")){
                 str.add(parameter[1]);
+                return "";
             }
         }
         if (url.getPath().contains("/search")){
@@ -18,12 +22,27 @@ class SearchEngine implements URLHandler{
                         result.add(str.get(i));
                     }
                 }
+                String output="";
                 for (int i=0; i<result.size(); i++){
-                    return result.get(i);
+                    output=output+result.get(i)+" ";
                 }
+                return output;
             }
         }
-        return "";
+        return "404 not found";
     }
     
 }
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
+
